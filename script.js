db446ba8059814de71952477a635923de983d9ce
ahmedbate55 <ahmedcomputerscience325@gmail.com>
2025-05-30 20:59:34 +0300

    this is the task:

---
+++ script.js
@@ -0,0 +1,6 @@
+let value = +prompt("enter the value: ");
+let offer = +prompt("enter the offer: ");
+let addtion = (offer / 100) * value;
+let result = addtion + value;
+console.log("your original value is: " + value);
+console.log("your value after addition is: " + result);
