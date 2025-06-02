813eedfe8a0d18382017397a03a95d4b87f921d9
ahmedbate55 <ahmedcomputerscience325@gmail.com>
2025-06-02 11:27:48 +0300

    the task:

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-let value = +prompt("enter the value: ");
-let offer = +prompt("enter the offer: ");
-let addtion = (offer / 100) * value;
-let result = addtion + value;
-console.log("your original value is: " + value);
-console.log("your value after addition is: " + result);
-
-
-
-
+// let value = +prompt("enter your value: ");
+// let discount = +prompt("enter the discaount: ");
+// let discountvalue = (discount / 100) * value;
+// let result = value - discountvalue;
+// let secdiscout = (14 / 100) * result;
+// let lastvalue = secdiscout + result;
+// console.log("your original price is: " + value);
+// console.log("your value after discount is: " + result);
+// console.log("you value ofter adding 14% is: "+lastvalue)
+// let firstname = prompt("enter your fisrt name: ");
+// let lastname = prompt("enter your last name: ");
+// let youryear = +prompt("enter you year: ");
+// let country = prompt("enter your country: ");
+// let calcyear = 2025 - youryear;
+// console.log("welcome: " + firstname + lastname);
+// console.log("your age is: " + calcyear);
+// console.log("you are from: " + country);
+// console.log("welcome: "+firstname+lastname+" your age is: "+calcyear+" "+"you are from: "+country)
+let merath = +prompt("enter the merath value: ");
+let woman_merath = (1 / 8) * merath;
+let almtbky = merath - woman_merath;
+let boys = 2;
+let girl = 1;
+let merathbnt = almtbky / (boys * 2) + girl;
+let boymerath = 2 * merathbnt;
+console.log("original merath: " + merath);
+console.log("merath al wife: " + woman_merath);
+console.log("merath al bnt: " + merathbnt);
+console.log("merath al boy: " + boymerath);
